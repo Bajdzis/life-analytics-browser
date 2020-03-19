61c6ea840b47bbcfe8cdc1e00de05073fd464228
Bajdzis <rafal612b@wp.pl>
2020-03-19 22:11:46 +0100

    feat: detect unactive

--- content.js
+++ content.js
@@ -1,25 +1,110 @@
-window.addEventListener('load', function() {
-    chrome.runtime.sendMessage({
-        type: 'load',
-        hostname: window.location.hostname,
-        pathname: window.location.pathname,
+(function() {
+    const FIVE_MINUTES = 300;
+    var themeColor = null;
+    var unActiveSeconds = 0;
+    var isActive = false;
+    var isFocus = false;
+
+    function getPayload() {
+        return {
+            host: window.location.host,
+            path: window.location.pathname,
+        }
+    }
+
+    function getTime() {
+        return Math.round((new Date()).getTime() / 1000);
+    }
+
+    function createEvent(type) {
+        const event = {
+            type,
+            time: getTime(),
+            payload: getPayload()
+        };
+
+        if(typeof themeColor === 'string' && themeColor.indexOf("#") === 0) {
+            event.color = themeColor;
+        }
+        console.log(event);
+        return event;
+    }
+
+    setTimeout(() => {
+        if (isActive) {
+            unActiveSeconds+=10;
+            if(unActiveSeconds > FIVE_MINUTES) {
+                isActive = false;
+                chrome.runtime.sendMessage(createEvent('stop.inactivity'));
+            }
+        }
+    }, 10000);
+
+    function activity() {
+        unActiveSeconds = 0;
+        if(isActive === false && isFocus === true) {
+            isActive = true;
+            chrome.runtime.sendMessage(createEvent('start.active'));
+        }
+    }
+ 
+    var activityEvents = [
+        'mousedown', 'mousemove', 'keydown', 
+        'scroll', 'touchstart', 'click'
+    ];
+ 
+    activityEvents.forEach(function(eventName) {
+        document.addEventListener(eventName, activity, true);
     });
-});
 
-window.addEventListener('focus', function() {
-    chrome.runtime.sendMessage({
-        type: 'focus',
-        hostname: window.location.hostname,
-        pathname: window.location.pathname,
+    let unregisterAllVideoPlayer = function() {};
+
+    function registerVideoPlayerActivities () {
+        unregisterAllVideoPlayer();
+
+        const videos = document.querySelectorAll('video');
+        videos.forEach((video) => {
+            video.addEventListener('timeupdate', activity, true);
+        })
+
+        unregisterAllVideoPlayer = function() {
+            videos.forEach((video) => {
+                video.removeEventListener('timeupdate', activity);
+            });
+        }
+
+    };
+
+    window.addEventListener('click', registerVideoPlayerActivities);
+
+    window.addEventListener('load', function() {
+        const meta = document.head.querySelector('meta[name="theme-color"]');
+        if (meta) {
+            themeColor = meta.content;
+        }
+        isActive = true;
+        chrome.runtime.sendMessage(createEvent('start.load'));
     });
-});
 
-window.addEventListener('beforeunload', function() {
-    chrome.runtime.sendMessage({
-        type: 'beforeunload',
-        hostname: window.location.hostname,
-        pathname: window.location.pathname,
+    window.addEventListener('focus', function() {
+        isActive = true;
+        isFocus = true;
+        chrome.runtime.sendMessage(createEvent('start.focus'));
     });
-});
 
+    window.addEventListener('blur', function() {
+        isFocus = false;
+        if (isActive === true) {
+            isActive = false;
+            chrome.runtime.sendMessage(createEvent('stop.blur'));
+        }
+    });
 
+    window.addEventListener('beforeunload', function() {
+        if (isActive === true) {
+            isActive = false;
+            chrome.runtime.sendMessage(createEvent('stop.unload'));
+        }
+    });
+
+})();
--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-chrome.runtime.onMessage.addListener(function (request) { 
+chrome.runtime.onMessage.addListener(function (event) { 
   chrome.storage.sync.get(['streamId'], function(result) {
     fetch(`https://auto-time-72f2c.firebaseio.com/streams/${result.streamId}/events.json`, {
       method:'POST',
-      body: JSON.stringify({
-          type: request.type, 
-          timestamp: (new Date()).getTime(),
-          payload: request
-      })
-    })
+      body: JSON.stringify(event)
+    }).then(console.log).catch(console.error)
   });
 });
